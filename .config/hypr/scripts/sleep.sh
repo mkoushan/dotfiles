14e79fcdc2e9606b0db2348d624677f7e0c4e870
mkoushan <mkoushan@proton.me>
2023-09-03 22:11:00 +0330

    Adding more

---
+++ .config/hypr/scripts/sleep.sh
@@ -0,0 +1,5 @@
+swayidle -w timeout 300 'swaylock -f -c 000000' \
+            timeout 500 'hyprctl dispatch dpms off' \
+            timeout 700 'systemctl suspend' \
+            resume 'hyprctl dispatch dpms on' \
+            before-sleep 'swaylock -f -c 000000' &
