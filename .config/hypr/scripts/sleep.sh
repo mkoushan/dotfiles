cd12c90ee831dc7dc5133522e4833e902c449bbb
mkoushan <mkoushan@proton.me>
2024-01-18 18:01:57 +0330

    Update

--- .config/hypr/scripts/sleep.sh
+++ .config/hypr/scripts/sleep.sh
@@ -1,7 +1,5 @@
 sudo pkill swayidle
 sudo pkill swaylock
 swayidle -w timeout 300 'swaylock -f -c 000000' \
-            timeout 500 'hyprctl dispatch dpms off' \
-            resume 'hyprctl dispatch dpms on' \
             timeout 700 'systemctl suspend' \
             before-sleep 'swaylock -f -c 000000' &
