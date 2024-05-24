de356214f0d1abedd6c82c565e67886146caa728
mkoushan <mkoushan@proton.me>
2024-05-24 19:52:05 +0330

    Update

--- .config/hypr/scripts/startup.sh
+++ .config/hypr/scripts/startup.sh
@@ -3,16 +3,11 @@ asusctl -c 80
 wpctl set-volume @DEFAULT_AUDIO_SINK@ 50%
 wpctl set-mute @DEFAULT_AUDIO_SOURCE@ 0
 
-#pkill telegram-desktop
-#sudo -EH telegram-desktop -- &
-
 pkill udiskie
 udiskie -ant &
 
 sudo pkill v2raya
 sudo -EH v2raya -- &
-
-swaybg -i ~/wp.jpg > /dev/null
 
 sleep 0.5 && $XDG_CONFIG_HOME/eww/scripts/init
 $HOME/.config/hypr/scripts/sleep.sh
