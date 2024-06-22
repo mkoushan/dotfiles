588d283f6c775db63b2a9002b2f09bf48b22baa6
mkoushan <mkoushan@proton.me>
2024-06-23 01:01:55 +0330

    Update

--- .config/hypr/scripts/startup.sh
+++ .config/hypr/scripts/startup.sh
@@ -6,8 +6,8 @@ wpctl set-mute @DEFAULT_AUDIO_SOURCE@ 0
 pkill udiskie
 udiskie -ant &
 
-pkill sslocal
-sslocal -b "127.0.0.1:1080" -c ~/config.json &
+pkill mihomo
+mihomo &
 
 sleep 0.5 && $XDG_CONFIG_HOME/eww/scripts/init
 $HOME/.config/hypr/scripts/sleep.sh
